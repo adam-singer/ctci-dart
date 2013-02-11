9a44f6c9f29620a0627bf883509934d963627e8b
adam <financeCoding@gmail.com>
2013-02-11 15:22:56 -0800

    Finishing off question 11

--- Chapter9/Question9_11/question.dart
+++ Chapter9/Question9_11/question.dart
@@ -97,8 +97,259 @@ bool isOperator(String c) {
   }
 }
 
+String insertParensAround(String expression, int ind) {
+  int left = 0;
+  int right = 0;
+  int index = 0;
+  int count = 0;
+  for (int i = 0; i < expression.length; i++) {
+    if (isOperator(expression[i])) {
+      if (count == ind) {
+        index = i;
+      }
+      count++;
+    }
+  }
+
+  count = 0;
+
+  for (int i = index - 1; i >= 0; i--) {
+    if (expression[i] == ')') {
+      count++;
+    } else if (expression[i] == '(') {
+      count--;
+    }
+
+    if (count == 0) {
+      left = i;
+      break;
+    }
+  }
+
+  count = 0;
+  for (int i = index + 1; i <= expression.length; i++) {
+    if (expression[i] == '(') {
+      count++;
+    } else if (expression[i] == ')') {
+      count--;
+    }
+
+    if (count == 0) {
+      right = i;
+      break;
+    }
+  }
+
+  if (left == 0 && right == expression.length - 1) {
+    return expression;
+  }
+
+  StringBuffer sb = new StringBuffer();
+  sb.add(expression.substring(0, left));
+  sb.add('(');
+  sb.add(expression.substring(left, right + 1));
+  sb.add(')');
+  sb.add(expression.substring(right + 1));
+  String newexpression = sb.toString();
+  return newexpression;
+}
+
+int bruteForce(String expression, Map<String, bool> completed, bool result, List<bool> flags) {
+  int count = 0;
+  bool isDone = true;
+  if (completed.containsKey(expression)) {
+    return 0;
+  }
+
+  for (int i = 0; i < flags.length; i++) {
+    if (!flags[i]) {
+      flags[i] = true;
+      String newexpression = insertParensAround(expression, i);
+      isDone = false;
+      count += bruteForce(newexpression, completed, result, flags);
+      flags[i] = false;
+    }
+  }
+
+  if (isDone) {
+    if (evaluate(expression, 0, expression.length - 1) == result) {
+      print("$expression = $result");
+      return 1;
+    } else {
+      print("$expression = $result");
+      return 0;
+    }
+  }
+
+
+  completed[expression] = true;
+  return count;
+}
+
+int countR(String exp, bool result, int start, int end) {
+
+  if (start == end) {
+    if (exp[start] == '1' && result) {
+      return 1;
+    } else if (exp[start] == '0' && !result) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  int c = 0;
+  if (result) {
+    for (int i = start + 1; i <= end; i += 2) {
+      String op = exp[i];
+      if (op == '&') {
+        c += countR(exp, true, start, i - 1) * countR(exp, true, i + 1, end);
+      } else if (op == '|') {
+        c += countR(exp, true, start, i - 1) * countR(exp, false, i + 1, end);
+        c += countR(exp, false, start, i - 1) * countR(exp, true, i + 1, end);
+        c += countR(exp, true, start, i - 1) * countR(exp, true, i + 1, end);
+      } else if (op == '^') {
+        c += countR(exp, true, start, i - 1) * countR(exp, false, i + 1, end);
+        c += countR(exp, false, start, i - 1) * countR(exp, true, i + 1, end);
+      }
+    }
+  } else {
+    for (int i = start + 1; i <= end; i += 2) {
+      String op = exp[i];
+      if (op == '&') {
+        c += countR(exp, false, start, i - 1) * countR(exp, true, i + 1, end);
+        c += countR(exp, true, start, i - 1) * countR(exp, false, i + 1, end);
+        c += countR(exp, false, start, i - 1) * countR(exp, false, i + 1, end);
+      } else if (op == '|') {
+        c += countR(exp, false, start, i - 1) * countR(exp, false, i + 1, end);
+      } else if (op == '^') {
+        c += countR(exp, true, start, i - 1) * countR(exp, true, i + 1, end);
+        c += countR(exp, false, start, i - 1) * countR(exp, false, i + 1, end);
+      }
+    }
+  }
+
+  return c;
+}
+
+int countDP(String exp, bool result, int start, int end, Map cache) {
+  String key = "${result}${start}${end}";
+  if (cache.containsKey(key)) {
+    return cache[key];
+  }
+
+  if (start == end) {
+    if (exp[start] == '1' && result == true) {
+      return 1;
+    } else if (exp[start] == '0' && result == false) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  int count = 0;
+  if (result) {
+    for (int i = start + 1; i <= end; i += 2) {
+      String op = exp[i];
+      if (op == '&') {
+        count += countDP(exp, true, start, i - 1, cache) * countDP(exp, true, i + 1, end, cache);
+      } else if (op == '|') {
+        count += countDP(exp, true, start, i - 1, cache) * countDP(exp, false, i + 1, end, cache);
+        count += countDP(exp, false, start, i - 1, cache) * countDP(exp, true, i + 1, end, cache);
+        count += countDP(exp, true, start, i - 1, cache) * countDP(exp, true, i + 1, end, cache);
+      } else if (op == '^') {
+        count += countDP(exp, true, start, i - 1, cache) * countDP(exp, false, i + 1, end, cache);
+        count += countDP(exp, false, start, i - 1, cache) * countDP(exp, true, i + 1, end, cache);
+      }
+    }
+  } else {
+    for (int i = start + 1; i <= end; i += 2) {
+      String op = exp[i];
+      if (op == '&') {
+        count += countDP(exp, false, start, i - 1, cache) * countDP(exp, true, i + 1, end, cache);
+        count += countDP(exp, true, start, i - 1, cache) * countDP(exp, false, i + 1, end, cache);
+        count += countDP(exp, false, start, i - 1, cache) * countDP(exp, false, i + 1, end, cache);
+      } else if (op == '|') {
+        count += countDP(exp, false, start, i - 1, cache) * countDP(exp, false, i + 1, end, cache);
+      } else if (op == '^') {
+        count += countDP(exp, true, start, i - 1, cache) * countDP(exp, true, i + 1, end, cache);
+        count += countDP(exp, false, start, i - 1, cache) * countDP(exp, false, i + 1, end, cache);
+      }
+    }
+  }
+
+  cache[key] = count;
+  return count;
+}
+
+int total(int n) {
+  // Function to return (2n)!/((n+1)! * n!)
+
+  // To keep the numbers small, we divided by i when possible to do evenly. If
+  // not, we store up the remainder and divide when possible.
+  int num = 1;
+  int rem = 1;
+  for (int i = 2; i <= n; i++) {
+    num *= (n + i);
+    rem *= i;
+    if (num % rem == 0) {
+      num ~/= rem;
+      rem = 1;
+    }
+  }
+
+  return num;
+}
+
+int countDPEff(String exp, bool result, int start, int end, Map<String, int> cache) {
+  String key = "${start}${end}";
+  int count = 0;
+  if (!cache.containsKey(key)) {
+    if (start == end) {
+      if (exp[start] == '1') {
+        count = 1;
+      } else {
+        count = 0;
+      }
+    }
+
+    for (int i = start + 1; i <= end; i += 2) {
+      String op = exp[i];
+      if (op == '&') {
+        count += countDPEff(exp, true, start, i - 1, cache) * countDPEff(exp, true, i + 1, end, cache);
+      } else if (op == '|') {
+        int left_ops = (i - 1 - start) ~/ 2; // parens on left
+        int right_ops = (end - i - 1) ~/ 2; // parens on right
+        int total_ways = total(left_ops) * total(right_ops);
+        int total_false = countDPEff(exp, false, start, i - 1, cache) * countDPEff(exp, false, i + 1, end, cache);
+        count += total_ways - total_false;
+      } else if (op == '^') {
+        count += countDPEff(exp, true, start, i - 1, cache) * countDPEff(exp, false, i + 1, end, cache);
+        count += countDPEff(exp, false, start, i - 1, cache) * countDPEff(exp, true, i + 1, end, cache);
+      }
+    }
+
+    cache[key] = count;
+  } else {
+    count = cache[key];
+  }
+
+  if (result) {
+    return count;
+  } else {
+    int num_ops = (end - start) ~/ 2;
+    return total(num_ops) - count;
+  }
+}
 
 
 void main() {
-  throw "SOLTION NOT FINISHED";
+  String terms = "0^0|1&1^1|0|1";
+  bool result = true;
+
+  bruteForce(terms, new Map<String, bool>(), result, new List<bool>.fixedLength((terms.length - 1) ~/ 2, fill: false));
+  print(countR(terms, result, 0, terms.length - 1));
+  print(countDP(terms, result, 0, terms.length - 1, new Map<String, int>()));
+  print(countDPEff(terms, result, 0, terms.length - 1, new Map<String, int>()));
 }
